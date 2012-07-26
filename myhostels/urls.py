4354c45a2b5007eba728989a8bfe97cf95b38172
william <omariwilliam2@gmail.com>
2012-07-26 09:46:38 +0000

    updated with bootstrap

--- myhostels/urls.py
+++ myhostels/urls.py
@@ -1,6 +1,5 @@
 from django.conf.urls import patterns, include, url
 from django.conf import settings
-
 # Uncomment the next two lines to enable the admin:
 from django.contrib import admin
 admin.autodiscover()
@@ -11,16 +10,9 @@ urlpatterns = patterns('',
     # url(r'^myhostels/', include('myhostels.foo.urls')),
 
     # Uncomment the admin/doc line below to enable admin documentation:
-<<<<<<< HEAD
-    url(r'^admin/doc/', include('django.contrib.admindocs.urls')),
+    # url(r'^admin/doc/', include('django.contrib.admindocs.urls')),
 
     # Uncomment the next line to enable the admin:
-    url(r'^admin/', include(admin.site.urls)),
-    url(r'^static/(?P<path>.*)$','django.views.static.serve',{'document_root':settings.STATIC_ROOT,}),
-=======
-     url(r'^admin/doc/', include('django.contrib.admindocs.urls')),
-
-    # Uncomment the next line to enable the admin:
-     url(r'^admin/', include(admin.site.urls)),
->>>>>>> 0bb5324c943e8a895b2e4e224d393b2d7f8a393e
+      url(r'^admin/', include(admin.site.urls)),
+      url(r'^static/(?P<path>.*)$','django.views.static.serve',{'document_root':settings.STATIC_ROOT,}),
 )
