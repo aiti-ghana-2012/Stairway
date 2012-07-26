e04d341c4c42caf69469488619414137b2022304
william <omariwilliam2@gmail.com>
2012-07-26 10:55:23 +0000

    changes made

--- hostels/views.py
+++ hostels/views.py
@@ -1,1 +1,24 @@
 # Create your views here.
+from django.forms import ModelForm
+from django.views.decorators.csrf import csrf_exempt
+from django.http import HttpResponseRedirect
+from django.template import Context, loader
+from django.http import HttpResponse
+
+from models import Hostel,Institution,Student,Rooms,Reservation,Amenities 
+from django.shortcuts import render_to_response
+
+def frontpage(request):
+    return render_to_response('hostels/frontpage.html',{})
+
+def hostels_list(request):
+    pass
+
+def hostels_detail(request,id,showparticularhostel):
+    pass
+
+
+
+
+def home(request):
+    return render_to_response('hostels/base.html',{})
--- myhostels/urls.py~
+++ myhostels/urls.py~
@@ -15,4 +15,5 @@ urlpatterns = patterns('',
     # Uncomment the next line to enable the admin:
       url(r'^admin/', include(admin.site.urls)),
       url(r'^static/(?P<path>.*)$','django.views.static.serve',{'document_root':settings.STATIC_ROOT,}),
+      url(r'^hostels/', include('hostels.urls')),
 )
