667b6af09b830be0e6c5340e1092a8b3f7116bf4
william <omariwilliam2@gmail.com>
2012-07-27 09:24:00 +0000

    project

--- hostels/urls.py
+++ hostels/urls.py
@@ -1,14 +1,14 @@
-"""
-This code should be copy and pasted into blog/urls.py
-"""
+
 from django.conf.urls.defaults import *
 
 urlpatterns = patterns('',
     url(r'^$', 'hostels.views.home'),
     url(r'^homepage/$', 'hostels.views.frontpage'),
     url(r'^homepage/displayhostels$', 'hostels.views.hostels_list'),
-    url(r'^homepage/displayhostels/(?P<id>\d+)/((?P<showparticularhostel>.*)/)?$','hostels.views.hostels_detail'),
-    
+    url(r'^homepage/displayhostels/AMEN$','hostels.views.AMENhostels_detail'),
+    url(r'^homepage/displayhostels/BRUNEI$','hostels.views.BRUNEIhostels_detail'),
+    url(r'^homepage/displayhostels/(?P<id>\d+)/((?P<hostelinfo>.*)/)?/studreg/$','hostels.views.studregister'),
+    url(r'^homepage/displayhostels/(?P<id>\d+)/((?P<hostelinfo>.*)/)?/studreg/studcon$','hostels.views.studconfirm'),
     
 )
 
