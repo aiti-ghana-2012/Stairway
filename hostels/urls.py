1e4af01be67a61803c4d2747e86b05748ef0379c
william <omariwilliam2@gmail.com>
2012-07-30 09:37:52 +0000

    updated Stairway

--- hostels/urls.py
+++ hostels/urls.py
@@ -4,9 +4,9 @@ from django.conf.urls.defaults import *
 urlpatterns = patterns('',
     url(r'^$', 'hostels.views.home'),
     url(r'^homepage/$', 'hostels.views.frontpage'),
-    url(r'^homepage/displayhostels$', 'hostels.views.hostels_list'),
-    url(r'^homepage/displayhostels/(?P<id>\d+)/((?P<hostelinfo>.*)/)?//$','hostels.views.hostels_detail'),
-    url(r'^homepage/displayhostels/(?P<id>\d+)/((?P<hostelinfo>.*)/)?/studreg/$','hostels.views.studregister'),
-    url(r'^homepage/displayhostels/(?P<id>\d+)/((?P<hostelinfo>.*)/)?/studreg/studcon$','hostels.views.studconfirm'),
+    url(r'^homepage/(?P<id>\d+)/displayhostels/$', 'hostels.views.hostels_list'),
+    url(r'^homepage/displayhostels/(?P<id>\d+)/$','hostels.views.hostels_detail'),
+    #url(r'^homepage/displayhostels/(?P<id>\d+)/((?P<hostelinfo>.*)/)?/studreg/$','hostels.views.studregister'),
+    #url(r'^homepage/displayhostels/(?P<id>\d+)/((?P<hostelinfo>.*)/)?/studreg/studcon$','hostels.views.studconfirm'),
     
 )
